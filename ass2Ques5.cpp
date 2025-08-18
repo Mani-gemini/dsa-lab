44bd9b11bd6980a4d84357dd3bc272ee29c0d475
Mani-gemini <manigemini1603@gmail.com>
2025-08-18 21:45:45 +0530

    Add files via upload

---
+++ ass2Ques5.cpp
@@ -0,0 +1,14 @@
+/*
+(1) Diagonal Matrix :- Store only the diagonal elements. 
+(2) Tri-diagonal Matrix:- Store the diagonal and the elements just above and below the diagonal. 
+(3) Lower triangular Matrix:- Store the diagonal and the elements below the diagonal.
+(4) Upper triangular Matrix:- Store the diagonal and the elements above the diagonal. 
+(5) Symmetric Matrix:- either like upper diagonal or lower diagonal */
+/*can store using row major or column major */
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout <<"hello"<<endl;
+    return 0;
+}
